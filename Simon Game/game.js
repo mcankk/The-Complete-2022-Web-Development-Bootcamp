f561bb9d710c4aa17734dafde6b0fe3dce2267ec
mcankk <32593826+mcankk@users.noreply.github.com>
2022-02-03 08:06:39 +0300

    Add files via upload

---
+++ Simon Game/game.js
@@ -0,0 +1,40 @@
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = [];
+var userClickedPattern= [];
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+    playSound(randomChosenColour);
+
+}
+
+
+
+
+$("btn").click(function(){
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+})
+
+
+function playSound(name){
+
+   
+        var audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+    
+    
+}
